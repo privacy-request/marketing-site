a118641ebafa683e423d165ded524daaac8d5437
Bradley Exton <bradleyexton@Bradleys-MacBook-Pro.local>
2021-01-20 16:06:48 -0500

    Install gatsby-source-prismic plugin nd add schemas for prismic data types

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,18 @@ module.exports = {
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: 'pr-marketing-site',
+        accessToken: 'MC5YXzBrLXhFQUFDRUFiTDVK.77-977-977-977-9QO-_vVvvv70277-9Ge-_vVnvv71HXe-_vRYjZO-_vSzvv73vv70vXu-_vVEuZ1xQ',
+        schemas: {
+          homepage: require("./src/schemas/homepage.json"),
+          navigation_bar: require("./src/schemas/navigation_bar.json"),
+          product_page: require("./src/schemas/product_page.json"),
+        },
+
+      },
+    },
   ],
 };
