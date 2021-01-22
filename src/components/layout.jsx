e83f2ddb5bc72e3b9f454f836a14016130be015a
Bradley Exton <bradleyexton@Bradleys-MacBook-Pro.local>
2021-01-22 13:09:37 -0500

    Create Layout component and add to homepage

---
+++ src/components/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const layout = ({ children }) => {
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+
+}
+
+
+export default layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import * as React from "react"
+import Layout from '../components/layout';
 
 const IndexPage = () => {
   return (
-    <main>
-
-    </main>
+    <Layout>
+    </Layout>
   )
 }
 
