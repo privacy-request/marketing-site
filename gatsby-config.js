4c0d3a95b7e9df2ea9af137d6f423ba117940315
BradleyExton <bradley@bradleyexton.ca>
2021-05-28 09:45:22 -0400

    remove old google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,20 +50,11 @@ module.exports = {
         googleAnalytics: {
           trackingId: "UA-154605502-2",
           cookieName: "gatsby-gdpr-google-analytics",
-          head: true,
           anonymize: true,
         },
-        environments: ["production"],
       },
     },
     "gatsby-plugin-styled-components",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-154605502-2",
-      },
-    },
-
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
