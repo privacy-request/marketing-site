ed7b78edf80c9dde4da945fd4097a10d48afb8d6
BradleyExton <bradley@bradleyexton.ca>
2021-12-08 07:35:59 -0800

    Remove unused code and packages leftover from cookie banner

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,7 +5,6 @@ import AppBar from "./AppBar/AppBar";
 import { ThemeProvider } from "styled-components";
 import theme from "../utils/theme";
 import Footer from "./Footer/Footer";
-import { CookiesProvider } from "react-cookie";
 import { Helmet } from "react-helmet";
 import "./font.css";
 
@@ -16,20 +15,18 @@ const OverflowWrapper = styled.main`
 const Layout = ({ children, path, navigationData, footerData, hideNavBar }) => {
   return (
     <ThemeProvider theme={theme}>
-      <CookiesProvider>
-        <Helmet>
-          <link rel="stylesheet" type="text/css" href="/fonts.css" />
-          <script
-            defer
-            data-domain="opsware.co"
-            src="https://plausible.io/js/plausible.js"
-          ></script>
-        </Helmet>
-        <GlobalStyles />
-        <AppBar path={path} {...navigationData} hideNavBar={hideNavBar} />
-        <OverflowWrapper>{children}</OverflowWrapper>
-        <Footer {...footerData} navItems={navigationData.body} />
-      </CookiesProvider>
+      <Helmet>
+        <link rel="stylesheet" type="text/css" href="/fonts.css" />
+        <script
+          defer
+          data-domain="opsware.co"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+      </Helmet>
+      <GlobalStyles />
+      <AppBar path={path} {...navigationData} hideNavBar={hideNavBar} />
+      <OverflowWrapper>{children}</OverflowWrapper>
+      <Footer {...footerData} navItems={navigationData.body} />
     </ThemeProvider>
   );
 };
